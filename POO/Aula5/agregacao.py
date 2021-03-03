bad1f4d8f6ccaf7c1531c1b17ad76f6ccb4e0b6b
Thyago A. Sanches <audisanches@gmail.com>
2021-03-03 14:57:05 -0300

    Aulas sobre python

---
+++ POO/Aula5/agregacao.py
@@ -0,0 +1,7 @@
+class Cliente:
+    def __init__(self,nome,telefone,cpf,endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.endereco = ender
+        
